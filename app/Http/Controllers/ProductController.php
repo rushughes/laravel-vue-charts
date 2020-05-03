cdeb6fb7c8d77bd15f99e14f0c5e0d28dca2d080
Rus Hughes <russell.hughes@gmail.com>
2020-05-03 12:31:44 +0200

    s1l5

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,15 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
     public function index() {
-      dd("Products");
+      $products = Product::orderBy('year', 'asc')->get();
+      return response()->json($products);
     }
 
-    public function store() {
+    public function store(Request $request) {
+      $product = new Product();
+      $product->name = $request->name;
+      $product->year = $request->year;
+      $product->price = $request->price;
+      $product->save();
 
+      return response()->json(['success' => 'The product was created']);
     }
 }
